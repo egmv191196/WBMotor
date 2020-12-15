47378afe8f9c808f3d104cf28a9c3f6aeb30d039
Edgar Gabriel Martinez Villegas <57368108+egmv191196@users.noreply.github.com>
2020-12-15 08:43:51 -0600

    Se agrego stack viewer y se agrego manualmente la carpeta de src/layout y la segunda ventana

---
+++ src/layouts/register.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Segunda ventana</Text>
+      <Button title="Toca aqui"/>
+      <Button title="Toca aqui"/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
